0ce92472ad15eefb9098e89b2b640555767940c2
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2015-03-29 08:52:19 -0700

    Formatierungsskript angepasst

--- belastung/format.sh
+++ belastung/format.sh
@@ -7,5 +7,5 @@ for FILE in $(ls | grep '^test_' | sed -e's/\.txt//g')
 do
     echo $FILE
     echo $FILE >> summary
-    sed -e 's/[[:alpha:]]\+\t//g' $FILE.txt | awk -F 'm' '{ split($2,s,"."); printf "%d.%s\n",$1*60+s[1],s[2] }' | sed -e 's/0.$//g' | tr '\n' ';' | sed -e 's/\;\;/\n/g' -e 's/^\;//' -e 's/\;$/\n/' -e 's/s//g' | tee $FILE.csv | awk -F ';' '{ print $1 }' | Rscript -e 'print(summary(scan("stdin")));' >> summary
+    grep 'real' $FILE.txt | sed -e 's/[[:alpha:]]\+\t//g' | awk -F 'm' '{ split($2,s,"."); printf "%d.%s\n",$1*60+s[1],s[2] }' | sed -e 's/s//g' | tee $FILE.csv | Rscript -e 'print(summary(scan("stdin")));' >> summary
 done
